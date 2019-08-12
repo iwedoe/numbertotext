f40b1a07c1adda3697624bc1d6886c687c74281f
Ivar Wedøe <iwedoe@gmail.com>
2019-08-12 20:01:45 +0200

    Adds 1

--- src/main/java/numbertotext/NumberToTextController.java
+++ src/main/java/numbertotext/NumberToTextController.java
@@ -12,6 +12,9 @@ public class NumberToTextController {
         int i;
         try {
             i = Integer.parseInt(number);
+            if (i == 1) {
+                return "Tallet er: en";
+            }
         } catch (NumberFormatException nfe) {
             return "Hvad fanden, mand! Jeg kender ikke det satans nummer!";
         }
